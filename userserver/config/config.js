1ce63afedeca7593af85a11cab6b3e7ccc986c36
sriram <sriramalavalapatiit01@gmail.com>
2023-02-24 22:49:28 +0530

    updated

--- userserver/config/config.js
+++ userserver/config/config.js
@@ -1,5 +1,5 @@
 const mongoose=require("mongoose");
 
-const connection=mongoose.connect("mongodb://127.0.0.1:27017/cwproject");
+const connection=mongoose.connect("use ur data base");
 
 module.exports={connection}
